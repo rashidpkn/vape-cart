49d8ac929e8cd93b2d150f72ec0f975fc60b8b89
rashidpkn <muhammed916rashid@gmail.com>
2024-03-25 12:15:06 +0530

    Somthing is added

--- frontend/dist/assets/use-params-7399a704.js
+++
@@ -1,1 +0,0 @@
-import{co as a,F as r}from"./index-df6fcad3.js";function e(){const s=a();return r.useMemo(()=>s,[s])}export{e as u};
---
+++ frontend/dist/assets/use-params-8fa97e5f.js
@@ -0,0 +1,1 @@
+import{co as a,F as r}from"./index-f9021dbd.js";function e(){const s=a();return r.useMemo(()=>s,[s])}export{e as u};
