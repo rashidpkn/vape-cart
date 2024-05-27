f69ad564879995751c86d813556ceb057d0f983a
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2024-05-27 16:31:43 +0000

    Ip changed

--- money/src/utils/api.js
+++ money/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://delhi-vape.com/',
+  baseURL: 'https://vape-amazon.com/',
   // baseURL:'/'
   // baseURL:'http://localhost:3000/'
 });
