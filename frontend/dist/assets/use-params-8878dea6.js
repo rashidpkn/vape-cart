cb8ea634f010960a00b321928303fa6eb416644a
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2024-05-27 16:48:39 +0000

    Rebuilded

--- frontend/dist/assets/use-params-8878dea6.js
+++
@@ -1,1 +0,0 @@
-import{dw as a,F as r}from"./index-25a65edf.js";function o(){const s=a();return r.useMemo(()=>s,[s])}export{o as u};
--- frontend/dist/assets/use-search-params-31cf9225.js
+++
@@ -1,1 +0,0 @@
-import{cT as r,F as s}from"./index-25a65edf.js";function c(){const[a]=r();return s.useMemo(()=>a,[a])}export{c as u};
