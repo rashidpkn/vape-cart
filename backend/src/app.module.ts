68b53af45010ae8ea6de1408e64ba9b47a3b6544
rashidpkn <muhammed916rashid@gmail.com>
2024-08-14 10:02:52 +0530

    file path

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { DatabaseModule } from './database/database.module';
 import { UploadModule } from './upload/upload.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientModule } from './client/client.module';
+
 import { OrdersModule } from './orders/orders.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { StoreAnalyticsModule } from './store-analytics/store-analytics.module';
